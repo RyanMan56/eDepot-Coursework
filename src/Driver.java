7d53a785697fd04c41d85c1edda6c1ee0c928b62
Bourban <jordanwalsh9519@gmail.com>
2016-03-22 18:47:42 +0000

    System Class Done
    System class finished
    Depot class started
    Driver class started

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,20 @@
-import java.util.List;
+import java.util.ArrayList;
 
 
 public class Driver {
 	protected String username, password;
-	private List<WorkSchedule> workSchedules;
+	private ArrayList<WorkSchedule> workSchedules;
 	
+	public Driver(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
 	
-	public boolean checkPassword(){
-		
+	public boolean checkPassword(String username, String password){
+		if((this.username == username) && (this.password == password))
+			return true;
+		else
+			return false;
 	}
 	
 	public boolean isAvailable(){
