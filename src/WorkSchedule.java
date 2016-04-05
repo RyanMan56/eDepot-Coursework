46aa5639376c038380140b741624d7c79b9cab77
Ryan Jones <RyanMan56@gmail.com>
2016-04-05 17:57:10 +0100

    WorkShedule

--- src/WorkSchedule.java
+++ src/WorkSchedule.java
@@ -4,5 +4,10 @@ import java.util.Date;
 public class WorkSchedule {
 	private String client;
 	private Date startDate, endDate;
-	
+
+	public WorkSchedule(String client, Date start, Date end){
+		this.client = client;
+		this.startDate = start;
+		this.endDate = end;
+	}
 }
